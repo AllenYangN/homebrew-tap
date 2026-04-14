37d6959a3e1f1797d676793d8a9b72bffd25966d
allen <641269549@163.com>
2026-04-14 11:07:41 +0800

    claude-status-light 0.1.1

--- Casks/claude-status-light.rb
+++ Casks/claude-status-light.rb
@@ -1,6 +1,6 @@
 cask "claude-status-light" do
-  version "0.1.0"
-  sha256 "19cdc69d7fef4d93a6f8c6b61cce0b37b687227ee818963270ee542c48a4212c"
+  version "0.1.1"
+  sha256 "8d3f7b26465181b3e1ad5b90d4a4e181eb64d51d85ad3cf4372a2e745580dd6b"
 
   url "https://github.com/AllenYangN/claude-status-light/releases/download/v#{version}/ClaudeStatusLight-#{version}.zip"
   name "Claude Status Light"
